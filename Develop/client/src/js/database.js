1c57dd779ee4e013acf4a2cf07a5f79709b82e87
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-03 20:06:39 +1000

    Update database.js
    Fixed putDB as it was going through delete instead

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.delete(id);
+  const request = store.put({id: id, todo: content});
   const result = await request;
   console.log('result.value', result);
 }
